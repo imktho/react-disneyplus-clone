1c91a316fea52c38e908d0d62c3d2bbc42f1a65a
Katie Ho <katherine.ho@outlook.com>
2021-11-29 14:54:21 -0600

    Add bg to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
     return (
-        <div>
+        <Container>
             Home
-        </div>
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    
+`
