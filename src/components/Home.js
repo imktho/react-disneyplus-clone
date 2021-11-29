5abb9f9ff0ff09a4332109edb868b976d4dc5f1b
Katie Ho <katherine.ho@outlook.com>
2021-11-29 16:37:42 -0600

    Add style to ImageSlider

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,7 @@ export default Home
 const Container = styled.div`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    overflow: hidden;
 
     background: url("/images/home-background.png") center center / cover no-repeat fixed;
     
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -11,7 +11,7 @@ function ImageSlider() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autplay: true
+        autoplay: true
     }
 
     return (
@@ -32,8 +32,40 @@ function ImageSlider() {
 export default ImageSlider
 
 const Carousel = styled(Slider)`
-    margin-top: 20px;
+    
+    ul li button{
+        &:before{
+            font-size: 10px;
+            color: white;
+        }
+    }
+
+    li.slick-active button:before{
+        color: white;
+    }
+
+    .slick-list{
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 
 const Wrap = styled.div`
+cursor: pointer;
+    img {
+        width: 100%;
+        height: 100%;
+        border: 4px solid transparent;
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 70%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 74%) 0px 16px 10px -10px;
+
+        &:hover{
+            transition-duration: 300ms;
+            border: 3px solid rgba(249, 249, 249, 0.8);
+        }
+    }
 `
