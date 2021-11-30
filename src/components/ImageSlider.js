446a3fbe141f911b254dd08873e2660e59161db5
Katie Ho <katherine.ho@outlook.com>
2021-11-30 10:19:45 -0600

    Add Viewers component

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -60,9 +60,10 @@ cursor: pointer;
         height: 100%;
         border: 4px solid transparent;
         border-radius: 4px;
-        box-shadow: rgb(0 0 0 / 70%) 0px 26px 30px -10px,
-        rgb(0 0 0 / 74%) 0px 16px 10px -10px;
-
+        box-shadow: 0px 26px 30px -10px rgba(0, 0, 0, 70%),
+        0px 16px 10px -10px rgba(0, 0, 0, 74%);
+        
+ 
         &:hover{
             transition-duration: 300ms;
             border: 3px solid rgba(249, 249, 249, 0.8);
